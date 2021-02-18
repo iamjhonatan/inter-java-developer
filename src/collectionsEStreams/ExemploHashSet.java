5bacd3c7cefea1fcf800357f99d94b4f289af6d2
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-02-17 21:33:00 -0300

    Exemplos HashSet e LinkedHashSet

---
+++ src/collectionsEStreams/ExemploHashSet.java
@@ -0,0 +1,70 @@
+package collectionsEStreams;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class ExemploHashSet {
+    /*
+    HashSet:
+     - Usa-se quando não é necessário manter uma ordenação;
+     - Não permite valores repetidos;
+     - Por não ser ordenado nem permitir repetição, é a implementação mais performática, quando falando da leitura dos dados.
+
+    TreeSet:
+     - Usa-se quando é necessário alterar a ordem, através do uso de comparators;
+     - Mantém ordem e pode ser reordenado;
+     - É performático para leitura. Para modificação tem a necessidade de reordenar, sendo mais lento que o LinkedHashSet;
+
+
+
+    java.util.Set
+
+    Por padrão, não garante ordem.
+    Não permite itens repetidos.
+    Permite a adição e remoção normalmente. Não possui busca por item e atualização. Para leitura, apenas navegação.
+    Não permite mudança de ordenação.
+     */
+
+    public static void main(String[] args) {
+
+        Set<Double> notasAlunos = new HashSet<>();
+
+        notasAlunos.add(5.8);
+        notasAlunos.add(9.3);
+        notasAlunos.add(6.5);
+        notasAlunos.add(10.0);
+        notasAlunos.add(5.4);
+        notasAlunos.add(7.3);
+        notasAlunos.add(3.8);
+        notasAlunos.add(4.0);
+
+        System.out.println(notasAlunos);
+
+        // Remove a nota do set
+        notasAlunos.remove(3.8);
+        System.out.println(notasAlunos);
+
+        // Retorna a quantidade de itens do set
+        System.out.println(notasAlunos.size());
+
+        // Navega em todos os itens do iterator
+        Iterator<Double> iterator = notasAlunos.iterator();
+
+        System.out.println("\nCom laço 'while': ");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("\nCom laço 'for': ");
+        for (Double nota: notasAlunos) {
+            System.out.println(nota);
+        }
+
+        // Limpando o set
+        notasAlunos.clear();
+
+        // Retorna se o set está vazio ou não
+        System.out.println("\nLista vazia? " + notasAlunos.isEmpty());
+    }
+}
---
+++ src/collectionsEStreams/ExemploLinkedHashSet.java
@@ -0,0 +1,50 @@
+package collectionsEStreams;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+public class ExemploLinkedHashSet {
+
+    /*
+    LinkedHashSet:
+     - Usa-se quando é necessário manter a ordem de inserção dos elementos;
+     - É a implementação mais lenta, por ser necessária manter a ordem dos dados.
+     */
+
+    public static void main(String[] args) {
+
+        LinkedHashSet<Integer> sequenciaNumerica = new LinkedHashSet<>();
+
+        sequenciaNumerica.add(13);
+        sequenciaNumerica.add(277);
+        sequenciaNumerica.add(1);
+        sequenciaNumerica.add(81);
+        sequenciaNumerica.add(16);
+
+        System.out.println(sequenciaNumerica);
+
+        // Remove o número o set
+        sequenciaNumerica.remove(4);
+        System.out.println(sequenciaNumerica);
+
+        // Retorna a quantidade de itens do set
+        System.out.println(sequenciaNumerica.size());
+
+        // Navega em todos os itens do iterator
+        Iterator<Integer> iterator = sequenciaNumerica.iterator();
+
+        System.out.println("\nExibindo elementos com laço 'while': ");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("\nExibindo os elementos com laço 'for': ");
+        for (Integer numero: sequenciaNumerica) {
+            System.out.println(numero);
+        }
+
+        // Retorna se o set está vazio ou não
+        System.out.println("\nLista vazia? " + sequenciaNumerica.isEmpty());
+
+    }
+}
